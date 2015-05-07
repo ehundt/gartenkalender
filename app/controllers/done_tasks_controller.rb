7b31e1fcb9e0364be74cf3cbec2bf980087cc235
Elisabeth Hundt <elisabeth.hoelscher@gmail.com>
2015-05-07 13:55:16 +0200

    display of current season added, startpage: dropbox removed and links added, skip a task added.

--- app/controllers/done_tasks_controller.rb
+++ app/controllers/done_tasks_controller.rb
@@ -4,9 +4,15 @@ class DoneTasksController < ApplicationController
   load_and_authorize_resource
 
   def create
-    @done_task = DoneTask.new(task_id: params[:task_id], season: 2)
+    dt_params = done_task_params.merge(task_id: params[:task_id], season: Season::current)
+    @done_task = DoneTask.new(dt_params)
+
     if @done_task.save
-      flash[:success] = "Aufgabe erfolgreich erledigt!"
+      if done_task_params[:skipped] == "true"
+        flash[:success] = "Aufgabe erfolgreich übersprungen."
+      else
+        flash[:success] = "Aufgabe erfolgreich erledigt!"
+      end
       redirect_to root_path
     end
   end
@@ -14,6 +20,6 @@ class DoneTasksController < ApplicationController
 private
 
   def done_task_params
-#    params.require(:task).permit(:task_id)
+    params.permit(:task_id, :skipped)
   end
 end
