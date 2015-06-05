3fe261d4f5f25cb415afebb73dc48c1228f5d6c2
Elisabeth Hundt <elisabeth.hoelscher@gmail.com>
2015-06-06 00:05:37 +0200

    Logged out page.

---
+++ app/assets/javascripts/logged_out.js
@@ -0,0 +1,3 @@
+.container-logged-out {
+  background: center repeat-x url('<%= image_url("rasen.png") %>');
+}
--- app/controllers/startpage_controller.rb
+++ app/controllers/startpage_controller.rb
@@ -2,8 +2,8 @@ class StartpageController < ApplicationController
   load_and_authorize_resource
 
   def index
-    unless current_user
-      render template: "startpage/logged_out/index"
+    if current_user.nil?
+      render layout: "logged_out", template: "startpage/logged_out/index"
     else
       @upcoming_tasks = Task.upcoming_tasks_for_user(current_user)
     end
