1f875733918aa81301f6ceb8bd97e0685a4f2d94
Elisabeth Hundt <elisabeth.hoelscher@gmail.com>
2015-06-07 12:51:15 +0200

    Typo: Tipps; sign_in form layout refactored; help and recos without authentication; user's list: also show number of private plants.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,10 +8,10 @@ module ApplicationHelper
   def help_content
     content = '<table class="table"><tr><td>'
     content += image_tag("world-globe32x32.png")
-    content += '</td><td>Zum Veröffentlichen dieser Pflanzentips auf dieses Icon klicken.'
+    content += '</td><td>Zum Veröffentlichen dieser Pflanzenbeschreibung auf dieses Icon klicken.'
     content += '</td></tr><tr><td>'
     content += image_tag "plant_active.gif", height: "42px", width: "30px"
-    content += '</td><td>Pflanze aktiv bzw. inaktiv schalten durch Klick auf die Pflanze. Nur die Pflegetips für aktive Pflanzen werden in der Aufgabenliste angezeigt.'
+    content += '</td><td>Pflanze aktiv bzw. inaktiv schalten durch Klick auf die Pflanze. Nur die Pflegetipps für aktive Pflanzen werden in der Aufgabenliste angezeigt.'
     content += '</td></tr><tr><td>'
     content += image_tag "thumb_up_not_voted.png"
     content += '</td><td>War diese Pflanzenbeschreibung besonders hilfreich? Dann klicke auf dieses Icon. Änderst Du Deine Meinung wieder, klicke ein zweites Mal.'
